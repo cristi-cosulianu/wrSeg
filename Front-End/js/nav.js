b02f527ef56f79d77641b21a3b50f5c966b080c8
Razdran <anton.razvan1997@gmail.com>
2018-11-09 20:09:45 +0200

    Template for front-end -> fixed
    SideNav is closing now

--- Front-End/js/nav.js
+++ Front-End/js/nav.js
@@ -2,12 +2,10 @@ function openNav(){
     document.getElementById("sideNav").style.width = "30%";
     document.getElementById("container").style.paddingRight = "30%";
     document.getElementById("container").style.width = "70%";
-    document.getElementById("open-sideNav").style.paddingRight = "30%";
 }
 function closeNav()
 {
-    document.getElementById("sideNav").style.display = "0";
+    document.getElementById("sideNav").style.width = "0%";
     document.getElementById("container").style.paddingRight = "0%";
     document.getElementById("container").style.width = "100%";
-    document.getElementById("open-sideNav").style.paddingRight = "0%";
 }
